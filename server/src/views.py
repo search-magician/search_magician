db1b15736f6c0fec6f7a3439a8a8a3641309ade8
mahmood sherif <mahmoodSherif13@gmail.com>
2021-02-08 01:56:12 +0200

    add youtube endpoint

--- server/src/views.py
+++ server/src/views.py
@@ -1,6 +1,25 @@
 from src import app
+import xml.etree.ElementTree as ET
+import requests
+import json
 
 
 @app.route("/")
 def index():
     return "Hello World!"
+
+
+@app.route("/vidoes/youtube/<videoId>")
+def youtube(videoId):
+    url = "http://video.google.com/timedtext?lang=en&v=" + videoId
+    response = requests.request("GET", url)
+    root = ET.fromstring(response.text)
+
+    res = []
+    for child in root:
+        print(child.text, child.get("dur"))
+        res.append(
+            {"start": child.get("start"), "dur": child.get("dur"), "text": child.text}
+        )
+
+    return json.dumps(res, indent=4)
