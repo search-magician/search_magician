90c6e0acd0b9382ac9949ad2cf3b492f522a7c3a
mahmood sherif <mahmoodSherif13@gmail.com>
2021-04-01 00:09:51 +0200

    convert from mp4 to mono wav

--- preprocessing/client/conv.py
+++ preprocessing/client/conv.py
@@ -1,6 +1,24 @@
-from pydub import AudioSegment
+import subprocess
 import sys
-sound = AudioSegment.from_mp3(sys.argv[1])
-fileName = sys.argv[1]
-newFileName = fileName[:-3] + "wav"
-sound.export(newFileName, format="wav")
+
+def check(name):
+    is_available = True
+    print('checking ' + name)
+    try:
+      print(subprocess.check_output(['which', name]))
+    except Exception as e:
+      print(e, e.output)
+      is_available = False
+    if not is_available:
+      print(f"Could not find {name}.  Please run 'sudo apt-get install {name}'.")
+
+
+if __name__ == "__main__":
+    check("ffmpeg")
+    fileName = sys.argv[1]
+    if(fileName == ""):
+        print("please add the file name as an arg to the script")
+
+    fileName = fileName.replace(' ','\ ')
+    command = f"ffmpeg -i {fileName} -ab 160k -ac 1 -ar 44100 -vn audio.wav"
+    subprocess.call(command, shell=True)
