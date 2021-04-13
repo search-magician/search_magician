7bf4aa78da7e283871fe55acee4406190a8481ff
mahmood sherif <mahmoodSherif13@gmail.com>
2021-04-13 20:37:54 +0200

    add entity recognition"

--- server/src/views.py
+++ server/src/views.py
@@ -1,6 +1,18 @@
 from src import app
-
+import spacy
+from flask import request
 
 @app.route("/")
 def index():
     return "Hello World!"
+
+
+@app.route("/er")
+def er():
+    nlp = spacy.load("en_core_web_trf")
+    strInput = str(request.data)
+    doc = nlp(strInput)
+    ans = ""
+    for ent in doc.ents:
+        ans += " <div> " + ent.text + " " + str(ent.start_char) + " " + str(ent.end_char) + " " + ent.label_ + " </div> "
+    return ans;
