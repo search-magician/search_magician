fe738141ae43f32ffc6172ce914061bd4bfede10
Waleed Badr <waleedehab99@gmail.com>
2021-05-03 16:57:26 +0200

    Added graph class

---
+++ scrapper/graph.py
@@ -0,0 +1,12 @@
+from collections import defaultdict
+
+class Graph:
+
+    # Constructor
+    def __init__(self):
+        # default dictionary to store graph
+        self.graph = defaultdict(list)
+
+    # function to add an edge to graph
+    def addEdge(self, u, v):
+        self.graph[u].append(v)
