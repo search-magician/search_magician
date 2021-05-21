e5804d66f9d99bcd87f4078d052bf772c715375d
Waleed Badr <waleedehab99@gmail.com>
2021-05-21 21:24:33 +0200

    Added heatmap figure

--- part matching/heatmap.py
+++ part matching/heatmap.py
@@ -25,3 +25,18 @@ def celanDataset(arr):
             lst = []
             c = 0
     return  arr
+
+def heatMap(arr):
+    sns.color_palette("rocket", as_cmap=True)
+    fig, ax = plt.subplots(figsize=(20, 150))
+
+    arr = np.array(arr)
+    arr = celanDataset(arr)
+
+    print(arr.shape)
+
+    df = pd.DataFrame({"Phrases": arr}, )
+
+    sns.heatmap(df, fmt="g", vmin=0, vmax=1,ax = ax,xticklabels=False)
+
+    plt.show()
