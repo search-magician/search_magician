90ebdec42484ff1ac1546f3e5ac81fd59fe83b8d
Waleed Badr <waleedehab99@gmail.com>
2021-05-21 21:28:57 +0200

    remove stopping words from search query

--- part matching/main.py
+++ part matching/main.py
@@ -16,4 +16,15 @@ for x in data:
     time.append(x['start'])
 
 
+def filteredSearch(str):
 
+    stop_words = set(stopwords.words('english'))
+    word_tokens = word_tokenize(str)
+    filtered_sentence = [w for w in word_tokens if not w in stop_words]
+    filtered_sentence = []
+    for w in word_tokens:
+        if w not in stop_words:
+            filtered_sentence.append(w)
+    print(word_tokens)
+    print(filtered_sentence)
+    return filtered_sentence
